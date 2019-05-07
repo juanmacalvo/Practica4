5f77b85bc2d7da68b62940a1b5fa315e736e86d5
juanmacalvo <48252840+juanmacalvo@users.noreply.github.com>
2019-05-07 18:41:52 +0200

    nuevas tablas sql

---
+++ tablas.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Usuario (
+id INT not null primary key
+GENERATED ALWAYS AS IDENTITY
+(START WITH 1, INCREMENT BY 1),
+nombre VARCHAR(40) not null unique,
+contrasena VARCHAR(40) not null unique
+
+);
+
+CREATE TABLE Cuenta (
+id INT not null primary key
+GENERATED ALWAYS AS IDENTITY
+(START WITH 1, INCREMENT BY 1),
+id_user INT not null,
+contrasena VARCHAR(60) not null,
+nombre VARCHAR(60) not null,
+
+CONSTRAINT userAjena1 FOREIGN KEY (id_user) REFERENCES Usuario (id) on delete cascade
+);
