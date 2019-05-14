a8929f0fa4459455cef91404911e8377859e50ed
usuario <usuario@pc21-086>
2019-05-14 17:32:23 +0200

    Implementacion de Crear nuevas claves. El usuario deberá suministrar el identificador de usuario, la clave y una pequeña descripción.

--- Contrasenna/src/main/java/com/mycompany/contrasenna/ManejaCuenta.java
+++ Contrasenna/src/main/java/com/mycompany/contrasenna/ManejaCuenta.java
@@ -5,6 +5,9 @@
  */
 package com.mycompany.contrasenna;
 
+import java.util.Scanner;
+import org.hibernate.HibernateException;
+
 /**
  *
  * @author usuario
@@ -13,9 +16,37 @@ public class ManejaCuenta extends Maneja{
     
     
     public void crearClave(){
+        
+        Scanner input = new Scanner(System.in);
+        String descrip = "";
+        String pass = "";
+        
+        
         System.out.println("Nueva Cuenta");
         System.out.println("Contraseña: ");
         
+        pass=input.next();
+        
+        System.out.println("Descripcion: ");
+        descrip=input.next();
+        
+        Cuenta cuenta=new Cuenta();
+        
+        cuenta.setIdUser(1);
+        cuenta.setContrasena(pass);
+        cuenta.setNombre(descrip);
+        
+        try {
+            iniciaOperacion();
+
+            getSesion().save(cuenta);
+
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+            throw he;
+        } finally {
+            finalizaOperacion();
+        }
         
     }
     
