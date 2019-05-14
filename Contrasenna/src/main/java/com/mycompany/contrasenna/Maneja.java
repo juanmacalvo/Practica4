58e08405d85ea458359ee1316dc63f67ae1b1822
usuario <usuario@pc21-086>
2019-05-14 16:38:41 +0200

    Nuevas clases encargadas de implementar las funcionalidades

---
+++ Contrasenna/src/main/java/com/mycompany/contrasenna/Maneja.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.contrasenna;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author usuario
+ */
+public class Maneja {
+
+    private Session sesion;
+    private Transaction tx;
+
+    public void iniciaOperacion() throws HibernateException {
+        System.out.println("Comenzando con Hibernate");
+        sesion = NewHibernateUtil.getSessionFactory().openSession(); //iniciamos una sesion hibernate
+        tx = sesion.beginTransaction(); // comienza la transaccion
+    }
+
+    public void finalizaOperacion() throws HibernateException {
+        System.out.println("Finalizando con Hibernate");
+        tx.commit();
+        sesion.close();
+    }
+
+    public void manejaExcepcion(HibernateException he) throws HibernateException {
+        tx.rollback();
+        System.out.println("Ocurrió un error en la capa de acceso a datos " + he.getMessage());
+        System.exit(0);
+    }
+}
