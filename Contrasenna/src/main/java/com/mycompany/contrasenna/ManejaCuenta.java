4812bfee8d8e0a4f130f8f8e0bca7b59699addc0
usuario <usuario@pc21-098>
2019-05-20 18:49:14 +0200

    Hast update

--- Contrasenna/src/main/java/com/mycompany/contrasenna/ManejaCuenta.java
+++ Contrasenna/src/main/java/com/mycompany/contrasenna/ManejaCuenta.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.contrasenna;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,6 +18,8 @@ import org.hibernate.HibernateException;
  */
 public class ManejaCuenta extends Maneja{
     
+       private static final char[] HEXADECIMAL = { '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
     
     public void crearClave(){
         
@@ -51,6 +55,25 @@ public class ManejaCuenta extends Maneja{
         }
         
     }
+     public String hash(String stringToHash) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] bytes = md.digest(stringToHash.getBytes());
+            StringBuilder sb = new StringBuilder(2 * bytes.length);
+            for (int i = 0; i < bytes.length; i++) {
+                int low = (int) (bytes[i] & 0x0f);
+                int high = (int) ((bytes[i] & 0xf0) >> 4);
+                sb.append(HEXADECIMAL[high]);
+                sb.append(HEXADECIMAL[low]);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            //exception handling goes here
+            return null;
+        }
+    }
+
+    
     
     public void listarClaves(){
         
